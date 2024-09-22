5218ecec1477f047989b7a2d6c29bee332276098
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-22 13:25:37 -0500

    first commit, initial set up of the react app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { PostsPage } from "./PostsPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <PostsPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/PostsPage.jsx
@@ -0,0 +1,7 @@
+export function PostsPage() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  );
+}
